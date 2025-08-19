d235ab4baadcf9534a051eb969d88ab14f2af956
Stefan Voorbij <svoorbij@gmail.com>
2025-08-19 22:34:43 +0200

    Nieuwe quote Maar niet van Cruijff

--- quotes.js
+++ quotes.js
@@ -38,6 +38,7 @@ module.exports = [
   "Rust is snelheid in vermomming.",
   "Een team wint niet omdat het beter is, maar omdat het beter samen is.",
   "Iedereen kan de juiste keuze maken, maar niet iedereen maakt ‘m op tijd.",
-  "Als je de bal niet hebt, kun je niet verliezen — maar je wint er ook niks mee."
+  "Als je de bal niet hebt, kun je niet verliezen — maar je wint er ook niks mee.",
+  "Alleen als je wint heb je vrienden."
 ];
 
